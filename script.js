72ee3ab74b7516e6b62e24f80949d3d89b7ae7f5
nobremariana <nobremariana9@gmail.com>
2024-02-08 14:12:38 -0300

    const-erro

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("funcionoiu:?")
+const convertButton = document.querySelector(".convertButton")
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor real
+    const currencyValueConverted = document.querySelector(".currency-value") // outras moedas
+
+    const dolarToday = 5.2
+
+    const convertedValues = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+
+    console.log(convertValues)
+}
+
+convertButton.addEventListener("click", convertValues)
